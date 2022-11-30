a18a0d91a863dec67b147130f4a4a338a1eee5e1
Kirans-143 <kiran.shet77@gmail.com>
2022-11-30 12:03:59 +0530

    Added home page with nodemon

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express"); //package t get the express object constructor
 const bodyParser = require("body-parser");
+const fs = require("fs");
 require("dotenv").config();
 
 const app = express(); //this function returns an express object which has the capabilities to handel the serverside codes
@@ -7,7 +8,14 @@ const app = express(); //this function returns an express object which has the c
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+app.set("views", "./views");
+app.set("view engine", "ejs");
+
 const PORT = process.env.PORT; //port on our local system
+
+app.get("/home", (req, res) => {
+  res.render("home");
+});
 
 app.listen(PORT, () => {
   //listen funregisters the app for the port
