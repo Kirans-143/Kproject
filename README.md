8c14e9ac38c391f282cec1d02a58f4805ab47b45
Kirans-143 <kiran.shet77@gmail.com>
2022-11-30 10:40:31 +0530

    Minor Dep: configured

--- README.md
+++ README.md
@@ -1,10 +1,16 @@
 E-commerce API project in NODEJS
 
--Requirements
+##---------------Requirements------------##
 -NodeJS
 -MySQL
 -NPM
 
--Setting up the project - Clone the repo
+##-----------Setting up the project------------##
+-Clone the repo
 -GO inside the clonned folder from your terminal/cmd
--run
+-run `npm install`
+-Create a new file `.env` and then configure the following enironment variables
+-PORT-3000
+
+##----------Run The Project------------##
+-Run `node server.js`
--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const express = require("express"); //package t get the express object constructor
+const bodyParser = require("body-parser");
+require("dotenv").config();
+
 const app = express(); //this function returns an express object which has the capabilities to handel the serverside codes
-const PORT = 3000; //port on our local system
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+const PORT = process.env.PORT; //port on our local system
+
 app.listen(PORT, () => {
   //listen funregisters the app for the port
   console.log(`server started on Port ${PORT}`);
